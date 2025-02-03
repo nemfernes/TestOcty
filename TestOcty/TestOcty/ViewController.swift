36d9dd3d55d3085c7710b4787be62f4d7cb07e12
Dmitry Kirpichev <dmitry.kirp@gmail.com>
2025-02-03 12:33:18 +0200

    init project

---
+++ TestOcty/TestOcty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestOcty
+//
+//  Created by Dmitry Kirpichev on 03.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
